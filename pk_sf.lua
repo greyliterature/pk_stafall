09f286f02defa51e7cafbcc699ef58eaf563a006
scripture <125283797+greyliterature@users.noreply.github.com>
2026-08-30 19:48:19 -0400

    Update pk_sf.lua

--- pk_sf.lua
+++ pk_sf.lua
@@ -41,9 +41,6 @@ if CLIENT then
     --]]-----------------------------------------
     local color_white = Color(255,255,255)
     local ang_zero = Angle(0, 0, 0)
-    if player() == owner() then
-        enableHud(owner(), true)
-    end
     --[[
     -- this doesnt work somehow
     hook.add("NetworkEntityCreated", "RetrackNewlyValidPlayers", function(ent)
@@ -51,80 +48,93 @@ if CLIENT then
         AccountedForPlayers[ent] = nil
     end)
     --]]
-    hook.add("PostDrawPlayer", "DevBox", function(ply, flags)
-        if not players[ply] then return end
-        local m = Matrix()
-        render.pushMatrix(m)
-        render.setColor(box_color)
-        local mins, maxs = ply:obbMins(), ply:obbMaxs()
-        render.draw3DWireframeBox(ply:getPos(), ang_zero, mins, maxs, true)
-        render.draw3DWireframeBox(ply:getPos(), ang_zero, mins, maxs, false) -- another one to fix Z write issues
-        render.popMatrix()
-    end)
-  hook.add("DrawHUD", "Nicks", function()
-        local playersindot = 0 
-        for _, ply in pairs(players) do
-            if ply == player() then continue end
-            local DistToTarg = (ply:getPos() - player():getShootPos()):getNormalized()
-            local dot = player():getAimVector():dot(DistToTarg)
-            if dot > min_dot then
-                playersindot = playersindot + 1  
-            end
-            if playersindot > max_players_in_dot then 
-                if dot < min_strict_dot then
-                    continue
-                end
-            end 
-            local toscreen = (ply:getPos()):toScreen()
-            local nick = ply:getName()
-            if #nick > 15 then 
-                nick = string.sub(nick, 1, 15) .. "..."
-            end 
-            render.setFont(nick_font)
-            render.setColor(nick_color)
-            render.drawSimpleText(toscreen.x, toscreen.y, nick, TEXT_ALIGN.CENTER) 
-        end
-    end)
-    
-    hook.add("Think", "Chams", function()
-        for _, ply in pairs(players) do
-            ply:setMaterial(player_cham_mat) -- have to always set their material, because theres seemingly no way to only set it once on EnterPVS
-            ply:setColor(cham_color)
-        end
-    end)
-    
-    hook.add("Removed", "ResetChams", function()
-        for _, ent in ipairs(find.all()) do
-            ent:setMaterial("")
-            ent:setColor(color_white)
-        end
-    end)
-    
-    --[[-----------------------------------------
-        Prop ESP
-    --]]-----------------------------------------
-    hook.add("OnEntityCreated", "prop_chams", function(ent)
-        if true then return end
-        if not isValid(ent) then return end
-        if ent:getClass() ~= "prop_physics" then return end
-        if timer.getTimersLeft() == 0 then return end
-        timer.simple(0.1, function()
-            if not isValid(ent) then return end
-            ent:setMaterial(prop_cham_mat)
-            ent:setColor(prop_color)
+    local function RunThings()
+        hook.add("PostDrawPlayer", "DevBox", function(ply, flags)
+            if not players[ply] then return end
+            local m = Matrix()
+            render.pushMatrix(m)
+            render.setColor(box_color)
+            local mins, maxs = ply:obbMins(), ply:obbMaxs()
+            render.draw3DWireframeBox(ply:getPos(), ang_zero, mins, maxs, true)
+            render.draw3DWireframeBox(ply:getPos(), ang_zero, mins, maxs, false) -- another one to fix Z write issues
+            render.popMatrix()
         end)
-    end)
-    --[[
-    hook.add("HUDConnected", "AddPlayerToTrack", function(ent, ply)
-        if ply == player() then printHud("HI TEST AAA") end
-        players[ply] = true
-    end)
-    hook.add("HUDDisconnected", "RemovePlayerToTrack", function(ent, ply)
-        players[ply] = nil
-    end)
-    --]]
+      hook.add("DrawHUD", "Nicks", function()
+            local playersindot = 0 
+            for ply, _ in pairs(players) do
+                if ply == player() then continue end
+                local DistToTarg = (ply:getPos() - player():getShootPos()):getNormalized()
+                local dot = player():getAimVector():dot(DistToTarg)
+                if dot > min_dot then
+                    playersindot = playersindot + 1  
+                end
+                if playersindot > max_players_in_dot then 
+                    if dot < min_strict_dot then
+                        continue
+                    end
+                end 
+                local toscreen = (ply:getPos()):toScreen()
+                local nick = ply:getName()
+                if #nick > 15 then 
+                    nick = string.sub(nick, 1, 15) .. "..."
+                end 
+                render.setFont(nick_font)
+                render.setColor(nick_color)
+                render.drawSimpleText(toscreen.x, toscreen.y, nick, TEXT_ALIGN.CENTER) 
+            end
+        end)
+        
+        hook.add("Think", "Chams", function()
+            if render.isHUDActive() ~= true then return end
+            for ply, _ in pairs(players) do
+                ply:setMaterial(player_cham_mat) -- have to always set their material, because theres seemingly no way to only set it once on EnterPVS
+                ply:setColor(cham_color)
+            end
+        end)
+        
+        hook.add("Removed", "ResetChams", function()
+            for _, ent in ipairs(find.all()) do
+                ent:setMaterial("")
+                ent:setColor(color_white)
+            end
+        end)
+        
+        hook.add("HUDDisconnected", "ResetChams", function(ent, ply)
+            for _, ent in ipairs(find.all()) do
+                ent:setMaterial("")
+                ent:setColor(color_white)
+            end
+        end)
+        
+        if player() == owner() then
+            enableHud(owner(), true)
+            RunThings()
+        end
+        --[[-----------------------------------------
+            Prop ESP
+        --]]-----------------------------------------
+        hook.add("OnEntityCreated", "prop_chams", function(ent)
+            if true then return end
+            if not isValid(ent) then return end
+            if ent:getClass() ~= "prop_physics" then return end
+            if timer.getTimersLeft() == 0 then return end
+            timer.simple(0.1, function()
+                if not isValid(ent) then return end
+                ent:setMaterial(prop_cham_mat)
+                ent:setColor(prop_color)
+            end)
+        end)
+        --[[
+        hook.add("HUDConnected", "AddPlayerToTrack", function(ent, ply)
+            if ply == player() then printHud("HI TEST AAA") end
+            players[ply] = true
+        end)
+        --]]
+    end
+    
     net.receive("UpdateTrackingTable", function()
         players = net.readTable()
+        RunThings()
     end)
 elseif SERVER then 
     local hud = prop.createComponent(chip():getPos(), Angle(0,0,0), "starfall_hud", "models/hunter/blocks/cube1x1x1.mdl", true)
